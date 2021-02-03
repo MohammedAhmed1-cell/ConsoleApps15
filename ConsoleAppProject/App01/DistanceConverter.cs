d77e468567f077926e18a6e3100dd59b3178088f
Mohammed Ahmed <71972232+MohammedAhmed1-cell@users.noreply.github.com>
2021-02-03 15:39:48 +0000

    Updated changes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,13 +1,54 @@
-﻿namespace ConsoleAppProject.App01
+﻿using System;
+
+namespace ConsoleAppProject.App01
 {
     /// <summary>
     /// Please describe the main features of this App
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Mohammed Ahmed version 0.1
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+        private double feet;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Run()
+        {
+            InputMiles();
+            CaluculateFeet();
+            OutputFeet();
+        }
+
+        /// <summary>
+        ///  Prompt the user to enter the miles
+        ///  Input the miles as a double number
+        /// </summary>
+        private void InputMiles()
+        {
+            Console.Write("Please enter the number of miles");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CaluculateFeet()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void OutputFeet()
+        {
+
+        }
 
     }
 }
